90900bc34090ae2bf3b337e040eb43f148f05c40
Premdass Ravidass <premdaz@amazon.co.uk>
2023-08-02 12:20:05 +0100

    Adding pre-commit with black plugin for python

--- server/app/models.py
+++ server/app/models.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 
 Base = declarative_base()
 
+
 class Book(Base):
     __tablename__ = "books"
 
@@ -12,6 +13,7 @@ class Book(Base):
     title = Column(String, index=True)
     author = Column(String, index=True)
     description = Column(String, index=True)
+
 
 class Event(BaseModel):
     message: str
@@ -27,6 +29,6 @@ class Event(BaseModel):
                 "status": "200 OK",
                 "type": "my.event.type",
                 "book": "1984",
-                "author": "George Orwell"
+                "author": "George Orwell",
             }
         }
