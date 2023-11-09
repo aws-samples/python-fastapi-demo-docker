6eeb1f4ac17fffa39076491a3d1a6ccd5f166881
Joe North <jbnorth@amazon.com>
2023-11-09 19:04:17 +0000

    Removed secrets cache and just using normal boto3.

--- server/app/connect.py
+++ server/app/connect.py
@@ -4,9 +4,9 @@ import logging
 from urllib.parse import urlparse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-import botocore
-import botocore.session
-from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
+import boto3
+from botocore.exceptions import ClientError
+import json
 
 def wait_for_db(db_url, max_retries=10, wait_time=1):
     logging.basicConfig(level=logging.INFO)
@@ -37,11 +37,42 @@ def wait_for_db(db_url, max_retries=10, wait_time=1):
 
     logger.error("Max retries reached. Unable to connect to the database.")
 
+# Retrieves secret by name from AWS Secrets Manager
+def get_secret(secret_name):
 
-client = botocore.session.get_session().create_client("secretsmanager")
-cache_config = SecretCacheConfig(refresh_interval=1)
-cache = SecretCache(client, cache_config)
-DATABASE_URL = cache.get_secret_string('eksdevworkshop-db-url')
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager'
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print("The requested secret " + secret_name + " was not found")
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            print("The request was invalid due to:", e)
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            print("The request had invalid params:", e)
+        elif e.response['Error']['Code'] == 'DecryptionFailure':
+            print("The requested secret can't be decrypted using the provided KMS key:", e)
+        elif e.response['Error']['Code'] == 'InternalServiceError':
+            print("An error occurred on service side:", e)
+    else:
+        # Secrets Manager decrypts the secret value using the associated KMS CMK
+        # Depending on whether the secret was a string or binary, only one of these fields will be populated
+        if 'SecretString' in get_secret_value_response:
+            return get_secret_value_response['SecretString']
+        else:
+            return get_secret_value_response['SecretBinary']
+
+secret_data = get_secret('eksdevworkshop-db-url')
+
+# Parse the json string in secret_data and extract connectionstring
+DATABASE_URL = json.loads(secret_data)['connectionstring']
+
 print("Database URL:", DATABASE_URL)
 
 wait_for_db(DATABASE_URL)
